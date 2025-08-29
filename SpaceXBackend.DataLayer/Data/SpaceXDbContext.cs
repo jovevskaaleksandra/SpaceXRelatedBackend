2dd41b0d5703970dc78e250d87b29a9eedda6e51
Aleksandra Jovevska <74111425+jovevskaaleksandra@users.noreply.github.com>
2025-08-29 14:03:13 +0200

    Add project files.

---
+++ SpaceXBackend.DataLayer/Data/SpaceXDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpaceXBackend.DataLayer.Models;
+
+namespace SpaceXBackend.DataLayer.Data
+{
+    public class SpaceXDbContext : DbContext
+    {
+        public SpaceXDbContext(DbContextOptions<SpaceXDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
---
+++ SpaceXBackend.DataLayer/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceXBackend.DataLayer.Models
+{
+    public class User
+    {
+        public int Id { get; set; } // rething if it should be int
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // why I chose UTCNow?
+    }
+}
